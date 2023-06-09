8f36a7c1a09b48109b5641f7d0341502af41ca18
Cartiilov <92887460+Cartiilov@users.noreply.github.com>
2023-06-09 12:05:31 +0200

    Created searchbar view without implementing actual searching functionality yet. Created a scrollable list of example results.

--- lib/ProductAddition.dart
+++ lib/ProductAddition.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'ShoppingList.dart';
+import 'products.dart';
 
 class ProductAddition extends StatelessWidget {
   const ProductAddition({super.key});
+
+  final List<Widget> products = const [
+    const Product(),
+    const Product(),
+    const Product(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +31,51 @@ class ProductAddition extends StatelessWidget {
             image: DecorationImage(
               image: AssetImage('assets/images/background.png'),
               fit: BoxFit.cover,
+
             ),
+          ),
+          constraints: const BoxConstraints.expand(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 40, bottom: 10),
+                width: 300,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 135, 223, 154),
+                    borderRadius: BorderRadius.circular(40),
+                ),
+                  child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                                Icons.search,
+                                size: 30),
+                            Flexible(
+                              child: TextField(),
+                            ),
+                          ]
+                      )
+                  )
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 40, right: 40, top: 40, bottom: 0),
+                width: 400,
+                height: 588,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(171, 135, 223, 154),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+                  ),
+                child: Center(
+                  child: ListView(
+                    children: products,
+                  ),
+                ),
+                )
+            ]
           ),
         ),
       ),
